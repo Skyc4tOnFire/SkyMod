44896a747ecbce100fbd4ad7f144f22d43fcef67
Skyc4tOnFire <rodrigo.marquina1@gmail.com>
2025-04-14 15:30:28 -0600

    Initial commit

---
+++ src/main/java/net/skyc4t/skymod/SkyModClient.java
@@ -0,0 +1,11 @@
+package net.skyc4t.skymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/skyc4t/skymod/SkyModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.skyc4t.skymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
